06e7a8b5cf5e40ecc18ba006a28d9a29e4e2564d
2014PC\Cesar Gonzalez <tankero@gmail.com>
2019-08-08 01:38:39 -0700

    Initial commit

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    private static LevelManager _instance;
+    public static LevelManager Instance { get; } = _instance;
+
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPlay()
+    {
+
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
+
+    public void OnMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+
+    public Button PlayButton;
+    public Button ScoreButton;
+    public Button QuitButton;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        LevelManager Manager = GameObject.Find("Level Manager").GetComponent<LevelManager>();
+        PlayButton.onClick.AddListener(() => Manager.OnPlay());
+
+        QuitButton.onClick.AddListener(() => Manager.OnQuit());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
