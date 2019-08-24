763a91387d79981a9698f0b1a13bfe94a5b671e9
kwlamb <kwlamb@gmail.com>
2019-08-24 16:28:37 -0700

    Picked up food now marks the grid square as no longer occupied.
    This allows food to respawn at a location after the food that was spawned there is picked up.

--- Assets/Scripts/FoodPickup.cs
+++ Assets/Scripts/FoodPickup.cs
@@ -6,15 +6,21 @@ public class FoodPickup : MonoBehaviour
 {
     [SerializeField] AudioClip foodPickupSFX;
     private bool addedToScore;
-
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Food being eaten by: " + collision.gameObject);
         if (!addedToScore)
         {
+            var contactPoint = collision.GetContact(0);
+
+            Vector2Int collisionPosition = 
+                new Vector2Int(
+                    (int)Mathf.Round(contactPoint.point.x), 
+                    (int)Mathf.Round(contactPoint.point.y));
+
             addedToScore = true;
-            FindObjectOfType<GameController>().OnSnakeCollisionWithFood();
+            FindObjectOfType<GameController>().OnSnakeCollisionWithFood(collisionPosition);
             //AudioSource.PlayClipAtPoint(foodPickupSFX, Camera.main.transform.position);//future implementation of an audio effect
             Destroy(gameObject);
         }
